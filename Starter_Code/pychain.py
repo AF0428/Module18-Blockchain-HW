12069b0a0b503bce25775054e341199b940d1364
Anthony Franco <anthonyfranco@stephennys-mbp.myfiosgateway.com>
2023-03-27 02:00:43 -0400

    final commit

--- Starter_Code/pychain.py
+++ Starter_Code/pychain.py
@@ -49,7 +49,11 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+@dataclass
+class Record:
+    sender: str
+    receivers: str
+    amount: float
 
 
 ################################################################################
@@ -170,15 +174,15 @@ input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender_data = st.text_input("Sender:")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver_data = st.text_input('Receiver:')
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount_data = st.text_input("Amount:")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,6 +193,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
+        record=Record(sender_data, receiver_data, amount_data),
         data=input_data,
         creator_id=42,
         prev_hash=prev_block_hash
